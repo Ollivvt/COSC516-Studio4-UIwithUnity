7c8c545e4945e21b73480ee3eaaf5995de90480b
Ollivvt <yutingtseng0203@gmail.com>
2025-03-11 01:21:55 -0700

    Created Tutorial Board

--- Assets/_Scripts/CoinCounterUI.cs
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -1,19 +1,21 @@
+using System.Collections;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
-using System.Collections;
 
-public class CoinCounterUI : MonoBehaviour {
+public class CoinCounterUI : MonoBehaviour
+{
     [SerializeField] private TextMeshProUGUI current;
     [SerializeField] private TextMeshProUGUI toUpdate;
     [SerializeField] private Transform coinTextContainer;
-    [SerializeField] private float duration = 0.4f;
+    [SerializeField] private float duration;
     [SerializeField] private Ease animationCurve;
 
-    private float containerInitPosition;
     private float moveAmount;
+    private float containerInitPosition;
 
-    private void Start() {
+    private void Start()
+    {
         Canvas.ForceUpdateCanvases();
         current.SetText("0");
         toUpdate.SetText("0");
@@ -21,16 +23,18 @@ public class CoinCounterUI : MonoBehaviour {
         moveAmount = current.rectTransform.rect.height;
     }
 
-    public void UpdateScore(int score) {
+    public void UpdateScore(int score)
+    {
         toUpdate.SetText($"{score}");
-        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(Ease.OutBounce);
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
         StartCoroutine(ResetCoinContainer(score));
     }
 
-    private IEnumerator ResetCoinContainer(int score) {
+    private IEnumerator ResetCoinContainer(int score)
+    {
         yield return new WaitForSeconds(duration);
         current.SetText($"{score}");
-        coinTextContainer.localPosition = new Vector3(
-            coinTextContainer.localPosition.x, containerInitPosition, coinTextContainer.localPosition.z);
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
     }
 }
